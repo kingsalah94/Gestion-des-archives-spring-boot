9002316da2e84e4e442d2a768b908a3137a07251
kingsalah94 <99025273+kingsalah94@users.noreply.github.com>
2024-06-09 15:25:25 +0100

    Creating git repository

---
+++ src/main/java/com/kingsamba/ged_archive/GedArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.kingsamba.ged_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingsamba/ged_archive/GedArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsamba.ged_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
